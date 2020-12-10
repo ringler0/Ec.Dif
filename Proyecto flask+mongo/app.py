8a5b7c4b3b7b53436d70928703ca76f2196ffdfb
nrivas2017 <31453370+nrivas2017@users.noreply.github.com>
2020-12-10 20:32:50 -0300

    asd

--- Proyecto flask+mongo/app.py
+++ Proyecto flask+mongo/app.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/Contaminacion'
-
 mongo = PyMongo(app)
+
+posts = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(posts))
 
 @app.route('/GetAll', methods=['GET'])
 def get_all_data():
