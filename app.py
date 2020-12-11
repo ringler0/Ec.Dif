eb2c108f6256c1766651631f5c0f4e6d5ef33bae
nrivas2017 <31453370+nrivas2017@users.noreply.github.com>
2020-12-11 00:10:13 -0300

    Resultado

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import render_template
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Contaminacion'
+mongo = PyMongo(app)
+
+API_KEY="AIzaSyAoPe9_VZO_2EmfopISDb3PKMqqAwmk1mM"
+posts = []
+
+
+def busca(id_auto,latitud,longitud):
+  autos = mongo.db.autos
+  if id_auto!="" or latitud!="" or longitud!="":
+    s = autos.find_one({'id':int(id_auto), 'lat' : str(latitud),'long':str(longitud)})
+  else:
+    s=0
+  if s:
+    output = {'id' : s['id'],'lat' : s['lat'], 'long' : s['long'], 'vel' : s['vel'], 'ang' : s['ang'], 'fecha' : s['fecha'], 'ign' : s['ign'], 'sat' : s['sat']}
+    b=1
+  else:
+    output = "No se ha encontrado coincidencia"
+    b=0
+  return output,b
+  #return jsonify({'result' : output})
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(posts))
+
+@app.route("/mostrar", methods=["POST"])
+def mapa():
+    req = request.form
+    id_auto = req['id']
+    latitud = str(req['lat'])
+    longitud = str(req['long'])
+    resultado,b = busca(id_auto,latitud,longitud)
+    if b:
+      return render_template("mapa.html",id_auto=resultado["id"],latitud=resultado["lat"],longitud=resultado["long"],velocidad=resultado["vel"],angulo=resultado["ang"],fecha=resultado["fecha"],ignicion=resultado["ign"],satelites=resultado["sat"])
+    else:
+      return resultado
+
+#@app.route('/GetAll', methods=['GET'])
+#def get_all_data():
+#  autos = mongo.db.autos
+#  output = []
+#  for s in autos.find():
+#    output.append({'id' : s['id'],'lat' : s['lat'], 'long' : s['long'], 'vel' : s['vel'], 'ang' : s['ang'], 'fecha' : s['fecha'], 'ign' : s['ign'], 'sat' : s['sat']})
+#  return jsonify({'result' : output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ ingresadb.py
@@ -0,0 +1,29 @@
+import csv,json
+import pymongo
+
+
+conexion = pymongo.MongoClient("localhost",27017)
+db = conexion["Contaminacion"]
+autos = db["autos"]
+"""
+1: id del vehículo, 
+2: latitud (GPS),
+3: longitud (GPS),
+4: velocidad
+5: ángulo de movimiento,
+6: fecha (día, hora, minutos, segundos)
+7: ignición (vehículo encendido o no) 
+8: número de satélites
+"""
+
+fData=open('./particion.csv','r', encoding='utf-8')
+aReader=csv.reader(fData)
+
+cont=1
+for row in aReader:
+    cont+=1
+    dic={"id":int(row[0]),"lat":row[1],"long":row[2],"vel":int(row[3]),"ang":int(row[4]),"fecha":row[5],"ign":int(row[6]),"sat":int(row[7])}
+    dic_json=json.dumps(dic, indent=4, ensure_ascii=False)
+    dato=json.loads(dic_json)
+    autos.insert_one(dato)
+    print(cont)
