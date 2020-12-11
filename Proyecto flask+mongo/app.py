ce14a38a4cfe7a44cff71be24c9c3904df503310
nrivas2017 <31453370+nrivas2017@users.noreply.github.com>
2020-12-10 22:09:34 -0300

    asd

--- Proyecto flask+mongo/app.py
+++ Proyecto flask+mongo/app.py
@@ -12,14 +12,16 @@ API_KEY="AIzaSyAoPe9_VZO_2EmfopISDb3PKMqqAwmk1mM"
 posts = []
 
 
-def busca(latitud,longitud):
+def busca(id_auto,latitud,longitud):
   autos = mongo.db.autos
-  s = autos.find_one({'lat' : str(latitud),'long':str(longitud)})
+  s = autos.find_one({'id':int(id_auto), 'lat' : str(latitud),'long':str(longitud)})
   if s:
     output = {'id' : s['id'],'lat' : s['lat'], 'long' : s['long'], 'vel' : s['vel'], 'ang' : s['ang'], 'fecha' : s['fecha'], 'ign' : s['ign'], 'sat' : s['sat']}
+    b=1
   else:
     output = "No se ha encontrado coincidencia"
-  return output
+    b=0
+  return output,b
   #return jsonify({'result' : output})
 
 
@@ -30,11 +32,14 @@ def index():
 @app.route("/mostrar", methods=["POST"])
 def mapa():
     req = request.form
-    #id_auto = req['id']
+    id_auto = req['id']
     latitud = str(req['lat'])
     longitud = str(req['long'])
-    resultado = busca(latitud,longitud)
-    return render_template("mapa.html",id_auto=resultado["id"],latitud=resultado["lat"],longitud=resultado["long"],velocidad=resultado["vel"],angulo=resultado["ang"],fecha=resultado["fecha"],ignicion=resultado["ign"],satelites=resultado["sat"])
+    resultado,b = busca(id_auto,latitud,longitud)
+    if b:
+      return render_template("mapa.html",id_auto=resultado["id"],latitud=resultado["lat"],longitud=resultado["long"],velocidad=resultado["vel"],angulo=resultado["ang"],fecha=resultado["fecha"],ignicion=resultado["ign"],satelites=resultado["sat"])
+    else:
+      return resultado
 
 #@app.route('/GetAll', methods=['GET'])
 #def get_all_data():
